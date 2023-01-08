3433a879a2fe0e808e6835c06750921ed5bcb670
Naman Singh <110779883+naman8102@users.noreply.github.com>
2023-01-08 21:35:07 +0530

    Add files via upload

---
+++ MEXOF2.C
@@ -0,0 +1,27 @@
+#include<conio.h>
+void main()
+{
+int num1,num2,r=0;
+clrscr();
+printf("Enter 1st number: ");
+scanf("%d",&num1);
+printf("Enter 2nd number: ");
+scanf("%d",&num2);
+
+if(num1==0 && num2==1 || num1==1 && num2==0)
+{
+r = r+2;
+printf("%d",r);
+}
+
+else if(num1==0 && num2>1 || num2==0 && num1>1)
+printf("%d",r+1);
+
+else if(num1==0 && num2==0)
+printf("%d",r=1);
+
+else
+printf("%d",r);
+
+getch();
+}
